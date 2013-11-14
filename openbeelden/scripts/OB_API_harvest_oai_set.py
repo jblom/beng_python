e266d183c4f0e992ba45355dc4ae18490f83b6b4
Jaap Blom <jblom@beeldengeluid.nl>
2013-11-14 13:50:32 +0100

    Updated the download script

--- openbeelden/scripts/OB_API_harvest_oai_set.py
+++ openbeelden/scripts/OB_API_harvest_oai_set.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 
 """
-OpenBeelden OAI harvester 
+Creator: 		Jaap Blom, Nederlands Instituut voor Beeld en Geluid
+License: 		GNU GPL v3, for a full copy of the license go to http://opensource.org/licenses/GPL-3.0
+Description: 	This script enables downloading a complete OAI set from http://www.openbeelden.nl/feeds/oai/ and saving it as an Excel file
 
-REQUIRED LIBRARIES:
-
-PyOAI: https://svn.infrae.com/pyoai/tag/pyoai-2.4/doc/API.html
-XLWT: 
-
+Required Python libraries:
+	PyOAI: https://svn.infrae.com/pyoai/tag/pyoai-2.4/doc/API.html
+	xlwt: https://pypi.python.org/pypi/xlwt
 """
+
 import xlwt
 from oaipmh.client import Client
 from oaipmh.metadata import MetadataRegistry, oai_dc_reader, MetadataReader
@@ -17,12 +18,13 @@ import itertools
 import codecs
 from dateutil.parser import parse
 
-OUTPUT_DIR = '/tmp' # change this directory
-OUTPUT_FILE = 'openbeelden_beeldengeluid_oai.xls' # change to whatever
-OAI_SET = u'beeldengeluid' # change the set (unless you want to harvest beeldengeluid ^^)
-EXCEL_SHEET_NAME = 'Sheet 1' # change if you care about the name of the Excel sheet
+URL = 'http://www.openbeelden.nl/feeds/oai/'
+OUTPUT_DIR = u'/tmp' # change to whatever
+OUTPUT_FILE = u'openbeelden_oai_set.xls' # change to whatever
+OAI_SET = u'beeldengeluid' # change the set (currently available: beeldengeluid, vpro, eclap, nimk, euscreen, rotterdam, stichting_natuurbeelden, UvA_Theaterwetenschap, eindhoven)
+EXCEL_SHEET_NAME = u'Sheet 1' # change to whatever
 
-#Een specifieke reader voor de oai_oi namespace van de Openbeelden OAI data provider
+"""Create a specific reader voor de oai_oi namespace"""
 oai_oi_reader = MetadataReader(
     fields={
     'title':       ('textList', 'oai_oi:oi/oi:title/text()'),
@@ -47,36 +49,26 @@ oai_oi_reader = MetadataReader(
     'license':      ('textList', 'oai_oi:oi/oi:license/text()')
     },
     
-    #TODO notitie maken van het feit dat oai_oi dezelfde ns heeft als oai_dc
     namespaces={
-    'oai_oi': 'http://www.openbeelden.nl/feeds/oai/', #'http://www.openarchives.org/OAI/2.0/oai_oi/',
+    'oai_oi': 'http://www.openbeelden.nl/feeds/oai/',
     'oi': 'http://www.openbeelden.nl/oai/'}
     )
 
-URL = 'http://www.openbeelden.nl/feeds/oai/'
-
-#Initieer de OAI client
+"""Initiate the OAI client"""
 registry = MetadataRegistry()
 registry.registerReader('oai_oi', oai_oi_reader)
 client = Client(URL, registry)
 x = client.updateGranularity()
 
-#Controleer of de OAI service goed geidentificeerd kan worden
+"""Check whether the service was successfully identified"""
 x = client.identify()
-print 'identity %s' % x.repositoryName()
-print 'identity %s' % x.protocolVersion()
-print 'identity %s' % x.baseURL()
+print 'repositoryName: %s' % x.repositoryName()
+print 'protocolVersion: %s' % x.protocolVersion()
+print 'baseURL: %s' % x.baseURL()
 
 def processOpenbeelden():
-	i=0
-	iarecs = []
-	extent = None
-	secs = 0
-	
 	wbk = xlwt.Workbook()
 	sheet = wbk.add_sheet(EXCEL_SHEET_NAME) 
-	i = 1
-	
 	sheet.write(0,0,'#')
 	sheet.write(0,1,'TITLE')
 	sheet.write(0,2,'ALTERNATIVE')
@@ -98,7 +90,9 @@ def processOpenbeelden():
 	sheet.write(0,18,'ATTRIBUTION NAME')
 	sheet.write(0,19,'ATTRIBUTION URL')
 	sheet.write(0,20,'LICENSE')
-		
+	
+	secs = 0
+	i = 1
 	for rec in client.listRecords(metadataPrefix=u'oai_oi', set=OAI_SET):
 		header, metadata, about = rec
 						
@@ -111,10 +105,8 @@ def processOpenbeelden():
 		publisher = getFieldData(metadata, 'publisher')
 		contributor = getFieldData(metadata, 'contributor')	
 		date = getFieldData(metadata, 'date')
-		type = getFieldData(metadata, 'type')
-		
+		type = getFieldData(metadata, 'type')		
 		extent = metadata.getField('extent')[0]
-		
 		medium = getFieldData(metadata, 'medium')
 		identifier = getFieldData(metadata, 'identifier')		
 		source = getFieldData(metadata, 'source')
@@ -149,17 +141,9 @@ def processOpenbeelden():
 		
 		i += 1				
 		secs += getExtentInSeconds(extent)
-	#393307
+
 	wbk.save('%s/%s' % (OUTPUT_DIR, OUTPUT_FILE))
-	print 'saved'
-	print secs
-	print 'TOTAL TIME: %s' % secsToTimeString(secs)
-	
-	#you can use this to list all the available sets
-	"""
-	for s in client.listSets():
-		print s
-	"""
+	print 'OAI set succesfully saved to %s/%s.\n\t Total duration (sec) of AV in set: %d' % (OUTPUT_DIR, OUTPUT_FILE, secs)
 
 def getFieldData(metadata, fn):
 	return '; '.join(metadata.getField(fn))
@@ -178,15 +162,13 @@ def getExtentInSeconds(ext):
 			secs += int(ext[0:ext.find('S')])
 	return secs
 
-def secsToTimeString(secs):
-	h = m = s = 0
-	while secs - 3600 >= 0:
-		h += 1
-		secs -= 3600	
-	while secs - 60 >= 0:
-		m += 1
-		secs -= 60
-	return '%d:%d:%d' % (h, m, s)
-	
-#Run the main function
+"""Prints all of the available sets in the OpenBeelden OAI"""
+def listOpenBeeldenSets():
+	for s in client.listSets():
+		print s
+
+"""This function can be used to print out all of the available OAI sets in OpenBeelden"""
+#listOpenBeeldenSets()
+
+"""Run the main function"""
 processOpenbeelden()
